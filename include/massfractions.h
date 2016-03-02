cfc934fb600b9f3808179b89ce3548c6eca17e0a
Bendik Samseth <b.samseth@gmail.com>
2016-03-02 16:23:56 +0100

    Added struct to hold information on mass fractions

---
+++ include/massfractions.h
@@ -0,0 +1,15 @@
+#ifndef MASSFRACTIONS_H
+#define MASSFRACTIONS_H
+
+struct MassFractions {
+  double X;
+  double Y3;
+  double Y4;
+  double Y;
+  double Z;
+  double Z_7Li;
+  double Z_7Be;
+};
+
+
+#endif
