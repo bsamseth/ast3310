fe44de90a62f92bf8a747203a7a8946a3f9f72b7
Bendik Samseth <b.samseth@gmail.com>
2016-03-03 20:31:56 +0100

    Lambda_ij should return in units m^3/s
    Was cm^3/s * N_A.

--- src/energy_production.cpp
+++ src/energy_production.cpp
@@ -11,12 +11,19 @@
 using namespace Particles;
 using namespace Lambdas;
 using namespace Reaction_energies;
+using std::cout;
+using std::endl;
 
 double energy(double T, double rho, MassFractions fractions) {
   double epsilon = 0;
+  double addon;
   for (int p1 = _e; p1 < N_PARTICLES; p1++) {
-    for (int p2 = Particle(p1+1); p2 < N_PARTICLES; p2++) {
-      epsilon += rate(Particle(p1), Particle(p2), fractions, T, rho) * Q[p1][p2];
+    for (int p2 = p1; p2 < N_PARTICLES; p2++) {
+      addon = rate(Particle(p1), Particle(p2), fractions, T, rho) * Q[p1][p2];
+      cout << "r_" << particle_names[p1] << particle_names[p2]
+	   << " Q_" << particle_names[p1] << particle_names[p2]
+	   << " rho = " << addon * rho << endl;
+      epsilon += addon;
     }
   }
   return epsilon;
@@ -24,7 +31,7 @@ double energy(double T, double rho, MassFractions fractions) {
 
 double rate(Particle p1, Particle p2, MassFractions fractions, double T, double rho) {
   return 1./ (rho * (1 + delta(p1, p2)))
-    * lambda[p1][p2](T) / Constants::N_A 
+    * lambda[p1][p2](T)
     * n_density(p1, fractions, rho)
     * n_density(p2, fractions, rho);
 }
--- src/functions.cpp
+++ src/functions.cpp
@@ -5,37 +5,37 @@ namespace Lambdas {
 
   std::function<double(double)> lambda_pp = [](double T) {
     double T9 = T * 1e-9;
-    return 4.01e-15 * pow(T9,-2./3) * exp(-3.380*pow(T9, -1./3)) *
-      (1 + 0.123*pow(T9, 1./3) + 1.09* pow(T9, 2./3) + 0.938*T9 );
+    return ( 4.01e-15 * pow(T9,-2./3) * exp(-3.380*pow(T9, -1./3)) *
+	     (1 + 0.123*pow(T9, 1./3) + 1.09* pow(T9, 2./3) + 0.938*T9 ) ) / Constants::N_A * Constants::M3_PER_CM3;
   };
 
   std::function<double(double)> lambda_33 = [](double T) {
     double T9 = T * 1e-9;
-    return 6.04e10 * pow(T9, -2./3) * exp(-12.276 * pow(T9, -1./3)) *
+    return ( 6.04e10 * pow(T9, -2./3) * exp(-12.276 * pow(T9, -1./3)) *
       (1 + 0.034 * pow(T9, 1./3) - 0.522 * pow(T9, 2./3) - 0.124 * T9 +
-       0.353 * pow(T9, 4./3) + 0.213 * pow(T9, -5./3));
+       0.353 * pow(T9, 4./3) + 0.213 * pow(T9, -5./3)) ) / Constants::N_A * Constants::M3_PER_CM3;
   };
 
   std::function<double(double)> lambda_34 = [](double T) {
     double T9 = T * 1e-9;
     double T9_star = T9 / (1 + 4.95e-2*T9);
-    return 5.61e6 * pow(T9_star, 5./6) * pow(T9, -3./2) *
-      exp(-12.826 * pow(T9_star, -1./3));
+    return ( 5.61e6 * pow(T9_star, 5./6) * pow(T9, -3./2) *
+	     exp(-12.826 * pow(T9_star, -1./3)) ) / Constants::N_A * Constants::M3_PER_CM3;
   };
 
   std::function<double(double)> lambda_e7Be = [](double T) {
     double T9 = T * 1e-9;
-    return 1.34e-10 * pow(T9,-1./2) * (1 - 0.537 * pow(T9, 1./3) +
+    return ( 1.34e-10 * pow(T9,-1./2) * (1 - 0.537 * pow(T9, 1./3) +
 				       3.86 * pow(T9, 2./3) +
-				       0.0027 / T9 * exp(2.515e-3/T9));
+					 0.0027 / T9 * exp(2.515e-3/T9)) ) / Constants::N_A * Constants::M3_PER_CM3;
   };
 
   std::function<double(double)> lambda_p7Li = [](double T) {
     double T9 = T * 1e-9;
     double T9_star = T9 / (1 + 0.0759*T9);
-    return 1.096e9 * pow(T9, -2./3) * exp(-8.472 * pow(T9, -1./3)) -
+    return ( 1.096e9 * pow(T9, -2./3) * exp(-8.472 * pow(T9, -1./3)) -
       4.830e8 * pow(T9_star, 5./6) * pow(T9, -3./2) * exp(-8.479 * pow(T9_star, -1./3)) +
-      1.06e10 * pow(T9, -3./2) * exp(-30.442/T9);
+	     1.06e10 * pow(T9, -3./2) * exp(-30.442/T9) ) / Constants::N_A * Constants::M3_PER_CM3;
   };
 
   std::function<double(double)> zero = [](double x) {return 0.0;};
