23a678bbe6784e273b8441b7e5ea1be0f8b1c73e
Bendik Samseth <b.samseth@gmail.com>
2016-03-03 19:26:38 +0100

    Added indexing to MassFractions struct
    Can now index on particle (MassFractions mf; mf[_33] -> Y_3)

--- include/massfractions.h
+++ include/massfractions.h
@@ -1,6 +1,8 @@
 #ifndef MASSFRACTIONS_H
 #define MASSFRACTIONS_H
 
+#include "constants.h"
+
 struct MassFractions {
   double X;
   double Y3;
@@ -9,6 +11,24 @@ struct MassFractions {
   double Z;
   double Z_7Li;
   double Z_7Be;
+  double fractions [Particles::N_PARTICLES] = { 0 };
+
+  void setFractions() {
+    fractions[Particles::_e]   = 0;
+    fractions[Particles::_p]   = X;
+    fractions[Particles::_3He] = Y3;
+    fractions[Particles::_4He] = Y4;
+    fractions[Particles::_7Li] = Z_7Li;
+    fractions[Particles::_7Be] = Z_7Be;
+  }
+
+  double operator[] (Particles::Particle p) {
+    return fractions[p];
+  }
+
+  double operator[] (int i) {
+    return fractions[i];
+  }
 };
 
 
