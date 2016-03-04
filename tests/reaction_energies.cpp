eeb879953cc0f6c735ac29be97a9e58d275802a0
Bendik Samseth <b.samseth@gmail.com>
2016-03-04 18:45:34 +0100

    Added tests for reaction_energies.h

---
+++ tests/reaction_energies.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+#include "reaction_energies.h"
+#include "particles.h"
+
+using namespace Particles;
+using namespace Reaction_energies;
+
+TEST(Reaction_energies, Q_indexes) {
+  EXPECT_EQ(Q_pp, Q[_p][_p]);
+  EXPECT_EQ(Q_33, Q[_3He][_3He]);
+  EXPECT_EQ(Q_34, Q[_3He][_4He]);
+  EXPECT_EQ(Q_e7Be, Q[_e][_7Be]);
+  EXPECT_EQ(Q_p7Li, Q[_p][_7Li]);
+  EXPECT_EQ(0, Q[_3He][_7Li]);
+  EXPECT_EQ(0, Q[_7Be][_e]);
+}
+
+TEST(Reaction_energies, Q_values) {
+  double eps = 1e-4;
+  EXPECT_NEAR(1.06705e-12, Q_pp, Q_pp*eps);
+  EXPECT_NEAR(2.0603991e-12, Q_33, Q_33*eps);
+  EXPECT_NEAR(2.547461e-13, Q_34, Q_34*eps);
+  EXPECT_NEAR(8.01088e-15, Q_e7Be, Q_e7Be*eps);
+  EXPECT_NEAR(2.779776e-12, Q_p7Li, Q_p7Li*eps);
+}
