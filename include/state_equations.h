ced055e5933aa0078f2f0d36beb8735c21bd853f
Bendik Samseth <b.samseth@gmail.com>
2016-03-17 10:36:54 +0100

    Wrapped state equations in namespace

--- include/state_equations.h
+++ include/state_equations.h
@@ -8,30 +8,35 @@
 #include "particles.h"
 #include "massfractions.h"
 
-/**
- * Return the mean molecular weight.
- * @param mf Mass fractions for the star
- * @return mu_0 - the mean molecular weight [dim. less]
- */
-double mu_0(MassFractions mf);
 
 /**
- * Return the density
- * @param T Temperature [K]
- * @param P Pressure [kg s^-2 m^-1]
- * @param mu_0 Mean molecular weight [dim. less]
- * @return rho - density [kg m^-3]
+ * Container for physical quantities related to the equation of state.
  */
-double rho(double T, double P, double mu_0);
+namespace StateEquations {
+  /**
+   * Return the mean molecular weight.
+   * @param mf Mass fractions for the star
+   * @return mu_0 - the mean molecular weight [dim. less]
+   */
+  double mu_0(MassFractions MF);
+
+  /**
+   * Return the density
+   * @param T Temperature [K]
+   * @param P Pressure [kg s^-2 m^-1]
+   * @param mu_0 Mean molecular weight [dim. less]
+   * @return rho - density [kg m^-3]
+   */
+  double rho(double T, double P, double mu_0);
 
 
-/**
- * Return the pressure
- * @param T Temperature [K]
- * @param rho Density [kg m^-3]
- * @param mu_0 Mean molecular weight [dim. less]
- * @return P - pressure [kg s^-2 m^-1]
- */
-double P(double T, double rho);
-
+  /**
+   * Return the pressure
+   * @param T Temperature [K]
+   * @param rho Density [kg m^-3]
+   * @param mu_0 Mean molecular weight [dim. less]
+   * @return P - pressure [kg s^-2 m^-1]
+   */
+  double P(double T, double rho, double mu_0);
+}
 #endif
--- src/state_equations.cpp
+++ src/state_equations.cpp
@@ -6,20 +6,22 @@
 using namespace Particles;
 
 
-/**
- * mu_0 = 1 / sum [ number of elements provided per nucleus X mass fraction / number of elements in core ]
- */
-double mu_0(MassFractions MF) {
-  double sum = 0;
-  for (int particle = _p; particle < N_PARTICLES; particle++) 
-    sum += (available_ionized_electrons[particle] + 1) * MF[particle] / core_elements[particle];
-  return 1./sum;
-}
+//namespace SateEquations {
+  /**
+   * mu_0 = 1 / sum [ number of elements provided per nucleus X mass fraction / number of elements in core ]
+   */
+double StateEquations::mu_0(MassFractions MF) {
+    double sum = 0;
+    for (int particle = _p; particle < N_PARTICLES; particle++) 
+      sum += (available_ionized_electrons[particle] + 1) * MF[particle] / core_elements[particle];
+    return 1./sum;
+  }
 
-double rho(double T, double P, double mu_0) {
-  return P * mu_0 * Constants::m_u / (rho * Constants::k_b * T);
-}
+double StateEquations::rho(double T, double P, double mu_0) {
+    return P * mu_0 * Constants::m_u / (Constants::k_b * T);
+  }
 
-double P(double T, double rho, double mu_0) {
-  return rho / (mu_0*Constants::m_u) * Constants::k_b * T;
-}
+double StateEquations::P(double T, double rho, double mu_0) {
+    return rho / (mu_0*Constants::m_u) * Constants::k_b * T;
+  }
+//}
