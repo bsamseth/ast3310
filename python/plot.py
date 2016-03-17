d358f830609025a409e78ceb509c3079c15a83c7
Bendik Samseth <b.samseth@gmail.com>
2016-03-17 21:44:06 +0100

    First version of plotter

---
+++ python/plot.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('../build/output.dat')
+
+m = data[:,0]
+r = data[:,1]
+P = data[:,2]
+L = data[:,3]
+T = data[:,4]
+rho = data[:,5]
+
+m /= m[0]
+r /= r[0]
+L /= L[0]
+rho /= rho[0]/5.1
+
+fig, ax = plt.subplots(2,2)
+
+ax[0,0].plot(m, r)
+ax[0,0].set_xlabel('M/M_0')
+ax[0,0].set_ylabel('R/R_0')
+
+ax[0,1].plot(m, L)
+ax[0,1].set_xlabel('M/M_0')
+ax[0,1].set_ylabel('L/L_0')
+
+ax[1,0].plot(m, T)
+ax[1,0].set_xlabel('M/M_0')
+ax[1,0].set_ylabel('T/T_0')
+
+ax[1,1].semilogy(m, rho)
+ax[1,1].set_xlabel('M/M_0')
+ax[1,1].set_ylabel('rho/rho_0*5.1')
+
+fig, ax = plt.subplots(2,2)
+
+ax[0,0].plot(r, m)
+ax[0,0].set_ylabel('M/M_0')
+ax[0,0].set_xlabel('R/R_0')
+
+ax[0,1].plot(r, L)
+ax[0,1].set_xlabel('R/R_0')
+ax[0,1].set_ylabel('L/L_0')
+
+ax[1,0].plot(r, T)
+ax[1,0].set_xlabel('R/R_0')
+ax[1,0].set_ylabel('T/T_0')
+
+ax[1,1].semilogy(r, rho)
+ax[1,1].set_xlabel('R/R_0')
+ax[1,1].set_ylabel('rho/rho_0*5.1')
+
+
+plt.show()
