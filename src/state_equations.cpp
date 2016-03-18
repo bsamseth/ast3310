952834fe30b922e1866aef09f61cdf4a3dcd74f1
Bendik Samseth <b.samseth@gmail.com>
2016-03-18 09:01:29 +0100

    Change equation of state to include radiation

--- src/state_equations.cpp
+++ src/state_equations.cpp
@@ -11,17 +11,17 @@ using namespace Particles;
    * mu_0 = 1 / sum [ number of elements provided per nucleus X mass fraction / number of elements in core ]
    */
 double StateEquations::mu_0(MassFractions MF) {
-    double sum = 0;
-    for (int particle = _p; particle < N_PARTICLES; particle++) 
-      sum += (available_ionized_electrons[particle] + 1) * MF[particle] / core_elements[particle];
-    return 1./sum;
-  }
+  double sum = 0;
+  for (int particle = _p; particle < N_PARTICLES; particle++) 
+    sum += (available_ionized_electrons[particle] + 1) * MF[particle] / core_elements[particle];
+  return 1./sum;
+}
 
 double StateEquations::rho(double T, double P, double mu_0) {
-    return P * mu_0 * Constants::m_u / (Constants::k_b * T);
-  }
+  return (P - Constants::a/3 * T*T*T*T) * mu_0 * Constants::m_u / (Constants::k_b * T);
+}
 
 double StateEquations::P(double T, double rho, double mu_0) {
-    return rho / (mu_0*Constants::m_u) * Constants::k_b * T;
-  }
+  return rho / (mu_0*Constants::m_u) * Constants::k_b * T + Constants::a/3 * T*T*T*T;
+}
 //}
