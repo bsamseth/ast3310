3eca8a018f6e5a18e4f01d98b15947f7e07cfd68
Bendik Samseth <b.samseth@gmail.com>
2016-03-29 19:38:53 +0200

    Plot with r as variable, not m

--- python/plot.py
+++ python/plot.py
@@ -13,43 +13,43 @@ rho = data[:,5]
 m /= m[0]
 r /= r[0]
 L /= L[0]
-rho /= rho[0]/5.1
-
-fig, ax = plt.subplots(2,2)
-
-ax[0,0].plot(m, r)
-ax[0,0].set_xlabel('M/M_0')
-ax[0,0].set_ylabel('R/R_0')
-
-ax[0,1].plot(m, L)
-ax[0,1].set_xlabel('M/M_0')
-ax[0,1].set_ylabel('L/L_0')
-
-ax[1,0].plot(m, T)
-ax[1,0].set_xlabel('M/M_0')
-ax[1,0].set_ylabel('T/T_0')
-
-ax[1,1].semilogy(m, rho)
-ax[1,1].set_xlabel('M/M_0')
-ax[1,1].set_ylabel('rho/rho_0*5.1')
+rho /= rho[0]
 
 fig, ax = plt.subplots(2,2)
 
 ax[0,0].plot(r, m)
-ax[0,0].set_ylabel('M/M_0')
-ax[0,0].set_xlabel('R/R_0')
+ax[0,0].set_ylabel(r'$M/M_0$')
+ax[0,0].set_xlabel(r'$R/R_0$')
 
 ax[0,1].plot(r, L)
-ax[0,1].set_xlabel('R/R_0')
-ax[0,1].set_ylabel('L/L_0')
+ax[0,1].set_xlabel(r'$R/R_0$')
+ax[0,1].set_ylabel(r'$L/L_0$')
 
 ax[1,0].plot(r, T)
-ax[1,0].set_xlabel('R/R_0')
-ax[1,0].set_ylabel('T/T_0')
+ax[1,0].set_xlabel(r'$R/R_0$')
+ax[1,0].set_ylabel(r'$T/T_0$')
 
 ax[1,1].semilogy(r, rho)
-ax[1,1].set_xlabel('R/R_0')
-ax[1,1].set_ylabel('rho/rho_0*5.1')
+ax[1,1].set_xlabel(r'$R/R_0$')
+ax[1,1].set_ylabel(r'$\rho/\rho_0$')
+
+# fig, ax = plt.subplots(2,2)
+
+# ax[0,0].plot(r, m)
+# ax[0,0].set_ylabel('M/M_0')
+# ax[0,0].set_xlabel('R/R_0')
+
+# ax[0,1].plot(r, L)
+# ax[0,1].set_xlabel('R/R_0')
+# ax[0,1].set_ylabel('L/L_0')
+
+# ax[1,0].plot(r, T)
+# ax[1,0].set_xlabel('R/R_0')
+# ax[1,0].set_ylabel('T/T_0')
+
+# ax[1,1].semilogy(r, rho)
+# ax[1,1].set_xlabel('R/R_0')
+# ax[1,1].set_ylabel('rho/rho_0*5.1')
 
 
 plt.show()
