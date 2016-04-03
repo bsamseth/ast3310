221e489ee43fc743bae6097387b1cd6db55bcab4
Bendik Samseth <b.samseth@gmail.com>
2016-04-03 16:14:25 +0200

    Better styling of plots.
    Use LaTeX-fonts, bigger labels ++.

--- python/plot.py
+++ python/plot.py
@@ -16,32 +16,35 @@ r /= r[0]
 L /= L[0]
 rho /= rho[0]
 P /= P[0]
+T /= T[0]
 
-fig, ax = plt.subplots(3,2)
+plt.rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+plt.rc('text', usetex = True)
+fig, ax = plt.subplots(3,2, sharex=True)
 
 ax[0,0].plot(r, m)
-ax[0,0].set_ylabel(r'$M/M_0$')
-ax[0,0].set_xlabel(r'$R/R_0$')
+ax[0,0].set_ylabel(r'$M/M_0$', size=18)
 
 ax[0,1].plot(r, T)
-ax[0,1].set_xlabel(r'$R/R_0$')
-ax[0,1].set_ylabel(r'$T/T_0$')
+ax[0,1].set_ylabel(r'$T/T_0$', size=18)
 
 ax[1,0].plot(r, L)
-ax[1,0].set_xlabel(r'$R/R_0$')
-ax[1,0].set_ylabel(r'$L/L_0$')
+ax[1,0].set_ylabel(r'$L/L_0$', size=18)
 
-ax[1,1].semilogy(r, eps)
-ax[1,1].set_xlabel(r'$R/R_0$')
-ax[1,1].set_ylabel(r'$\epsilon$')
+skipfrac = 1000 # 1/skipfrac first values skiped (because of very small numbers)
+eps = eps[len(eps)//skipfrac:]
+eps /= eps[0]
+reps = r[len(eps)//skipfrac:]
+ax[1,1].semilogy(reps, eps)
+ax[1,1].set_ylabel(r'$\epsilon/\epsilon_0$', size=20)
 
 ax[2,0].semilogy(r, P)
-ax[2,0].set_xlabel(r'$R/R_0$')
-ax[2,0].set_ylabel(r'$P/P_0$')
+ax[2,0].set_xlabel(r'$R/R_0$', size=18)
+ax[2,0].set_ylabel(r'$P/P_0$', size=18)
 
 ax[2,1].semilogy(r, rho)
-ax[2,1].set_xlabel(r'$R/R_0$')
-ax[2,1].set_ylabel(r'$\rho/\rho_0$')
+ax[2,1].set_xlabel(r'$R/R_0$', size=18)
+ax[2,1].set_ylabel(r'$\rho/\rho_0$', size=18)
 
 
 # fig, ax = plt.subplots(2,2)
