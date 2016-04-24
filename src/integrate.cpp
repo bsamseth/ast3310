baefd449efef8d6195e2f8a18bc70d7c31226f9d
Bendik Samseth <b.samseth@gmail.com>
2016-04-24 16:31:23 +0200

    :sparkles: Added first version of integrating with convection
    Not clear if this is correct. Testing must be done, and the maths
    should be gone over once more.
    
    In particular, fixing the geometri constant needs to be thought through.

--- src/integrate.cpp
+++ src/integrate.cpp
@@ -12,6 +12,7 @@
 #include "state_equations.hpp"
 #include "integrate.hpp"
 #include "opacity.hpp"
+#include "thirdordersolver.hpp"
 
 
 using namespace Constants;
@@ -69,6 +70,36 @@ std::string Integrate::integrate(double L_0, double T_0, double P_0, double rho_
       dms[3] = abs(p_max*T / dTdm);
       dms[4] = abs(p_max*m);
       dm = - (*min_element(dms, dms+5));
+    }
+
+    double nabla_radiative = nabla_rad(T, rho, L, P, m, kappa);
+    double nabla_adiabatic = nabla_ad();
+    if (nabla_radiative > nabla_adiabatic) {
+        double alpha = 1.0; // parameter, should be arond 1.
+        double H_p = P * r*r / (rho * G * m);
+        double l_m = alpha * H_p;
+        double delta = 1; // - dln rho / dln T = 1, ideal gass
+        double c_p = 5./2 * k_b / (StateEquations::mu_0(MF) * m_u); // TODO: Check formula for c_p
+        double g = G * m / (r*r);
+
+        double U = 64 * sigma * T*T*T / (3 * kappa(T, rho) * rho*rho * c_p)
+                        * sqrt(H_p / (g * delta));
+
+        double xi;
+        {
+            double U_lm2 = U / (l_m*l_m);
+            double G = 1; // TODO: Find geometric constant
+            G = 2. / (l_m*l_m);
+            double a = 1;
+            double b = U_lm2;
+            double c = U_lm2 * U * G;
+            double d = - U_lm2 * (nabla_radiative - nabla_adiabatic);
+            double* x = new double[3];
+            solve3order(x, a, b, c, d);
+            xi = x[0];
+        }
+        double nabla = xi*xi + U*G * xi + nabla_adiabatic;
+        dTdm = - T * G * m / (4 * pi * r*r*r*r * P) * nabla;
     }
 
     r += drdm * dm;
