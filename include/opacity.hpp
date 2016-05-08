a9061d6dfd1ea8b66b2a0b121915c79be7952068
Bendik Samseth <b.samseth@gmail.com>
2016-05-09 00:38:23 +0200

    :books: Update Opacity documentation
    Added extrapolation, so this is mentioned

--- include/opacity.hpp
+++ include/opacity.hpp
@@ -12,11 +12,11 @@ const std::string opacities_filename = "opacity.txt";
  * in a matrix. The object can then be used as a function, preforming a bilinear
  * interpolation of the data matrix in order to provide the estimated value.
  * Using an object enables the user to only load the data once, causing a
- * significant increase in speed, compared to the alliterative.
+ * significant increase in speed, compared to the alternative.
  */
 class Opacity {
  public:
-  
+
   /**
    * Instantiates an object using the default data file.
    */
@@ -25,7 +25,9 @@ class Opacity {
   /**
    * Return the estimated opacity value.
    * Use bilinear interpolation to find the value best suited to the inputs.
-   * Extrapolating is currently not available, and behaviour is undefined for this case.
+   * Extrapolating will be preformed if needed, where new points are taken
+   * based on a linear gradient on the two closest data points, in each
+   * extrapolation direction.
    *
    * @param T Temperature [K]
    * @param rho Density [kg m^-3]
@@ -37,7 +39,7 @@ class Opacity {
   /*
    * The remaining methods are helpers used in the public methods.
    */
-  
+
   int find_closest_index(double a, arma::vec A);
   double interp2d(double q11, double q12, double q21, double q22, double x1, double x2, double y1, double y2, double x, double y);
   arma::vec read_log10R();
